a8f265057362afd55efa78da6082fb6ddcd1d102
enidev911 <enidev911@gmail.com>
2023-02-03 17:51:40 -0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
 
 const expand = (pic) => {
 	Swal.fire({
-	  title: 'Custom animation with Animate.css',
-	  showClass: {
-	    popup: 'animate__animated animate__fadeInDown'
-	  },
-	  hideClass: {
-	    popup: 'animate__animated animate__fadeOutUp'
-	  }
+		title: 'Custom animation with Animate.css',
+		showClass: {
+			popup: 'animate__animated animate__fadeInDown'
+		},
+		hideClass: {
+			popup: 'animate__animated animate__fadeOutUp'
+		}
 	})
 }
 
 window.expand = expand;
+
+// EMAILJS
+(function () {
+	emailjs.init("vr5VeZVQQcOECM0u");
+})();
+
+emailjs.send("default_service")
+
