d3a4b10360899af6b2143001fc01fc9d59f7de6d
enidev911 <enidev911@gmail.com>
2023-02-05 03:57:08 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 
 const expand = (pic) => {
 	Swal.fire({
-		title: 'Custom animation with Animate.css',
+		imageUrl: pic,
+  		imageAlt: 'Custom image',
+		confirmButtonText: "Cerrar",
 		showClass: {
 			popup: 'animate__animated animate__fadeInDown'
 		},
@@ -12,6 +14,26 @@ const expand = (pic) => {
 }
 
 window.expand = expand;
+
+// MESSAGES 
+function showMessage() {
+	const Toast = Swal.mixin({
+		toast: true,
+		position: 'top-end',
+		showConfirmButton: false,
+		timer: 3000,
+		timerProgressBar: true,
+		didOpen: (toast) => {
+			toast.addEventListener('mouseenter', Swal.stopTimer)
+			toast.addEventListener('mouseleave', Swal.resumeTimer)
+		}
+	})
+
+	Toast.fire({
+		icon: 'success',
+		title: 'Se envió su mensaje correctamente'
+	})
+}
 
 // EMAILJS
 
@@ -29,17 +51,22 @@ function sendEmail() {
 	emailjs.send("service_3ez48bg", "template_voxdrdo", contactParams, "vr5VeZVQQcOECM0u-")
 		.then((res) => {
 			console.log('EXITO', res.status, res.text);
-			alert("ENVIADO");
-		},(error) => {
+			showMessage();
+		}, (error) => {
 			console.log('FAILED...', error);
-		 })
+		})
 		.catch((err) => {
 			console.log(err.message)
 		})
 }
 
-document.getElementById("form").addEventListener("submit", function(event){
+document.getElementById("form").addEventListener("submit", function (event) {
 	event.preventDefault();
 	sendEmail();
-  });
+});
 
+document.getElementById("poster-image").onclick = function () { fadeImage() };
+
+function fadeImage() {
+	document.getElementById("poster-image").style.opacity = "0";
+}
