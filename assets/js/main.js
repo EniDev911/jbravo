527ee503d24d08c22efef1f7d6a15b0fbd0f9d94
marco <enidev911@gmail.com>
2024-02-13 12:52:50 -0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// MESSAGES 
+// MESSAGES
 function showMessage() {
 	const Toast = Swal.mixin({
 		toast: true,
@@ -20,38 +20,38 @@ function showMessage() {
 
 // LIMPIAR CAMPOS
 function cleanBox(){
-		const name = document.getElementById("name"),
-		email = document.getElementById("email"),
-		phone = document.getElementById("phone"),
-		message = document.getElementById("message")
-		name.value = ''
-		email.value = ''
-		phone.value = ''
-		message.value = ''
+	const name = document.getElementById("name"),
+	email = document.getElementById("email"),
+	phone = document.getElementById("phone"),
+	message = document.getElementById("message")
+	name.value = ''
+	email.value = ''
+	phone.value = ''
+	message.value = ''
 }
 // EMAILJS
 function sendEmail() {
 	const name = document.getElementById("name").value,
-		email = document.getElementById("email").value,
-		phone = document.getElementById("phone").value,
-		message = document.getElementById("message").value,
-		contactParams = {
-			name,
-			email,
-			phone,
-			message
-		}
+	email = document.getElementById("email").value,
+	phone = document.getElementById("phone").value,
+	message = document.getElementById("message").value,
+	contactParams = {
+		name,
+		email,
+		phone,
+		message
+	}
 	emailjs.send("service_3ez48bg", "template_voxdrdo", contactParams, "vr5VeZVQQcOECM0u-")
-		.then((res) => {
-			console.log('EXITO', res.status, res.text);
-			showMessage();
-			cleanBox();
-		}, (error) => {
-			console.log('FAILED...', error);
-		})
-		.catch((err) => {
-			console.log(err.message)
-		})
+	.then((res) => {
+		console.log('EXITO', res.status, res.text);
+		showMessage();
+		cleanBox();
+	}, (error) => {
+		console.log('FAILED...', error);
+	})
+	.catch((err) => {
+		console.log(err.message)
+	})
 }
 
 document.getElementById("form").addEventListener("submit", function (event) {
@@ -59,8 +59,3 @@ document.getElementById("form").addEventListener("submit", function (event) {
 	sendEmail();
 });
 
-document.getElementById("poster-image").onclick = function () { fadeImage() };
-
-function fadeImage() {
-	document.getElementById("poster-image").style.opacity = "0";
-}
